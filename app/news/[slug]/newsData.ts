bc6b687865254b6764b12501608f0ff869e991d3
muhnovi <muhnoviyanto815@gmail.com>
2025-12-24 23:09:48 +0700

    update fix share di berita umkm

--- app/news/[slug]/newsData.ts
+++ app/news/[slug]/newsData.ts
@@ -13,16 +13,18 @@ export interface NewsItem {
 }
 
 export const newsData: NewsItem[] = [
-  // {
-  //   date: "24 Desember 2025",
-  //   title: "Klethik Klethik Ibu Suyatmi",
-  //   description:
-  //     "Klethik Klethik adalah usaha yang berbahan utama jagung",
-  //   fullDescription:
-  //     "Klethik Klethik adalah usaha yang berbahan utama jagung. Usaha ini didirikan oleh Ibu Suyatmi pada tahun 2010. Produk utama dari usaha ini adalah klethik klethik, yaitu camilan tradisional yang terbuat dari jagung yang diolah dengan cara khusus untuk menghasilkan rasa yang gurih dan renyah. Usaha ini telah berkembang pesat dan kini menjadi salah satu usaha kecil menengah yang terkenal di Dukuh Garotan.",
-  //   image: "/farmer-training-program-indonesia.jpg",
-  //   youtubeUrl: "https://www.youtube.com/watch?v=example1",
-  //   slug: "program-pemberdayaan-petani-dimulai",
-  //   location: { lat: -7.858661511592048, lng: 110.72306997857926 },
-  // },
+  {
+    date: "24 Desember 2025",
+    title: "Klethik Klethik Ibu Suyatmi",
+    description:
+      "UMKM lokal yang mengembangkan aneka snack tradisional berbasis jagung dan singkong seperti keripik pisang, marning, dan manggleng.",
+    fullDescription:
+      "UMKM ini dirintis sejak tahun 1992 dengan modal awal sebesar Rp50.000 dan terus berkembang hingga saat ini.\n\nJenis usaha yang dijalankan meliputi produksi aneka snack tradisional antara lain keripik pisang gurih, keripik pisang manis, marning jagung gurih, marning pedas manis, serta manggleng gurih dan manggleng manis. Selain itu, UMKM ini juga memproduksi lemper, risol, sarang burung (tahu isi telur puyuh), nogosari, apem jagung, tiwul instan, dan tepung jagung alami tanpa bahan pengawet.\n\nPemasaran dilakukan dengan sistem penjualan langsung ke wilayah Yogyakarta, Bandung, dan pasar tradisional. Pembayaran masih dilakukan secara tunai, sementara pada kegiatan pameran telah digunakan barcode sebagai media informasi produk.\n\nSelain usaha olahan pangan, UMKM ini juga mengelola peternakan kambing putih sebanyak 32 ekor dan ayam sebagai usaha pendukung. Dengan pengalaman lebih dari tiga dekade, UMKM ini berkomitmen melestarikan pangan tradisional serta meningkatkan nilai tambah hasil pertanian lokal.",
+    image: "/klethik-klethik.jpeg",
+    youtubeUrl: "https://www.youtube.com/embed/example1",
+    slug: "program-pemberdayaan-petani-dimulai",
+    location: { lat: -7.8414561, lng: 110.712003 },
+    locationName: "Klethik Klethik Ibu Suyatmi",
+    mapsUrl: "https://www.google.com/maps/search/?api=1&query=Klethik+Klethik+IBU+SUYATMI",
+  },
 ]
