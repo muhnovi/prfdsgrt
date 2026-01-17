86957f859114ed1a4efd7a53f6606981d441b5c4
muhnovi <muhnoviyanto815@gmail.com>
2026-01-17 21:23:41 +0700

    update berita 17 desember

--- lib/berita-terkini-data.ts
+++ lib/berita-terkini-data.ts
@@ -18,14 +18,14 @@ export const beritaData: BeritaItem[] = [
     image: "/foto-berita/kkntdutabangsa.png",
     slug: "kknt-universitas-duta-bangsa-surakarta-dukuh-garotan",
   },
-   {
+  {
     date: "10 Desember 2025",
     title: "KKNT Universitas Duta Bangsa Surakarta di Dukuh Garotan",
     description:
       "Mahasiswa KKNT Universitas Duta Bangsa Surakarta melaksanakan kegiatan pengabdian masyarakat bersama warga Dukuh Garotan, Kalurahan Bendung, Kapanewon Semin, Gunungkidul.",
     fullDescription:
       "Kegiatan Kuliah Kerja Nyata Tematik (KKNT) Universitas Duta Bangsa Surakarta dilaksanakan pada tanggal 10 Desember 2025 di Dukuh Garotan, Kalurahan Bendung, Kapanewon Semin, Kabupaten Gunungkidul. Kegiatan ini bertujuan untuk mempererat hubungan antara mahasiswa dan masyarakat sekaligus memberikan kontribusi nyata dalam pembangunan sosial dan edukasi desa.\n\nRangkaian kegiatan diawali dengan sambutan dari perangkat desa dan tokoh masyarakat setempat, dilanjutkan dengan program kerja mahasiswa KKNT seperti sosialisasi pendidikan, pendampingan belajar anak-anak, serta kegiatan kebersihan lingkungan bersama warga. Antusiasme masyarakat terlihat dari partisipasi aktif dalam setiap kegiatan yang dilaksanakan.\n\nSelain itu, mahasiswa KKNT juga mengadakan kegiatan sosial dan edukatif yang berfokus pada peningkatan kesadaran masyarakat akan pentingnya gotong royong, pendidikan, dan kesehatan lingkungan. Melalui kegiatan ini, diharapkan terjalin kerja sama yang berkelanjutan antara Universitas Duta Bangsa Surakarta dan masyarakat Dukuh Garotan guna mendukung kemajuan desa secara berkelanjutan.",
-    image: "/foto-berita/kkntdutabangsa.png",
+    image: "/images(3).jpg",
     slug: "kknt-universitas-duta-bangsa-surakarta-dukuh-garotan",
   },
 
